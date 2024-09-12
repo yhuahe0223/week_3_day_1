9fa210a55d6ffbf7ad26d132114af5678b142f2f
He <yhuahe@cps.k12.il.us>
2024-09-12 14:43:00 -0500

    math

--- week_3_day_1.py
+++ week_3_day_1.py
@@ -186,34 +186,34 @@
 
 
 ##########################################################################################
-# #addition
-print(2+1)
-# #multiplication
-print(2*2)
-# #division
-print(3/2)
-# #modulo
-print(3%2)
-# #powers
-print(2**3)
-# #get the max and min of a number
-print(max(1,2)) # max number get the largest number 
-print(min(1,2)) # min number get the smallest number 
-# #round a number
-print(round(3.4))
-# # absolute value
-print(abs(-3)) # the distance between the value to zero, always positive 
-# # order of operations
-print(2+10/3*5)
-# #to do more you need to import special math libraries from python
-from math import *    
-# #this goes out and grabs some different math functions we can use
-# #floor method
-print(floor(3.7))
-# #ceil method
-print(ceil(2.7))
-# #sqrt method
-print(sqrt(36))
+# # #addition
+# print(2+1)
+# # #multiplication
+# print(2*2)
+# # #division
+# print(3/2)
+# # #modulo
+# print(3%2)
+# # #powers
+# print(2**3)
+# # #get the max and min of a number
+# print(max(1,2)) # max number get the largest number 
+# print(min(1,2)) # min number get the smallest number 
+# # #round a number
+# print(round(3.4))
+# # # absolute value
+# print(abs(-3)) # the distance between the value to zero, always positive 
+# # # order of operations
+# print(2+10/3*5)
+# # #to do more you need to import special math libraries from python
+# from math import *    
+# # #this goes out and grabs some different math functions we can use
+# # #floor method
+# print(floor(3.7))
+# # #ceil method
+# print(ceil(2.7))
+# # #sqrt method
+# print(sqrt(36))
 
 
 
@@ -241,7 +241,7 @@ num1 = input("enter a number")
 num2 = input("enter another number")
 # # print out a statement where you:
 # # add them together
-print(num1 + num2)
+print(num1+num2)
 # #multiply
 print(num1*num2)
 # # find the max number
